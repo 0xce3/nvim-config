d2d75a06011cce173cafb0593ff8c2c06c19d1d7
Daniel Carius <daniel.carius@codewrights.de>
2026-06-09 11:01:14 +0200

    Add Telescope word search mapping

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -811,6 +811,7 @@ return {
       map("n", "<C-p>",      builtin.find_files,  { desc = "Find files" })
       map("n", "<leader>ff", builtin.find_files,  { desc = "Find files" })
       map("n", "<leader>fg", live_grep,           { desc = "Live grep" })
+      map("n", "<leader>fw", builtin.grep_string, { desc = "Find word under cursor" })
       map("n", "<leader>fb", builtin.buffers,     { desc = "Find buffers" })
       map("n", "<leader>fh", builtin.help_tags,   { desc = "Help tags" })
       map("n", "<leader>fr", builtin.oldfiles,    { desc = "Recent files" })
