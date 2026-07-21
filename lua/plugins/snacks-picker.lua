e5c8427a04a379aa40ddb1a2b6d0df34db2b68e6
Daniel Carius <daniel.carius@codewrights.de>
2026-07-21 12:34:44 +0200

    fix: scroll picker previews with shifted arrows

--- lua/plugins/snacks-picker.lua
+++ lua/plugins/snacks-picker.lua
@@ -8,10 +8,14 @@ return {
       opts.picker.win.input.keys = opts.picker.win.input.keys or {}
       opts.picker.win.input.keys["<S-j>"] = { "preview_scroll_down", mode = { "n", "i" } }
       opts.picker.win.input.keys["<S-k>"] = { "preview_scroll_up", mode = { "n", "i" } }
+      opts.picker.win.input.keys["<S-Down>"] = { "preview_scroll_down", mode = { "n", "i" } }
+      opts.picker.win.input.keys["<S-Up>"] = { "preview_scroll_up", mode = { "n", "i" } }
       opts.picker.win.list = opts.picker.win.list or {}
       opts.picker.win.list.keys = opts.picker.win.list.keys or {}
       opts.picker.win.list.keys["<S-j>"] = "preview_scroll_down"
       opts.picker.win.list.keys["<S-k>"] = "preview_scroll_up"
+      opts.picker.win.list.keys["<S-Down>"] = "preview_scroll_down"
+      opts.picker.win.list.keys["<S-Up>"] = "preview_scroll_up"
     end,
   },
 }
