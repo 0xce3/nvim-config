197d599a7b30327c46b208a03d13535931fb2321
Daniel Carius <daniel.carius@codewrights.de>
2026-07-30 09:43:47 +0200

    fix: avoid lazygit keymap conflict

--- lua/plugins/gitgraph.lua
+++ lua/plugins/gitgraph.lua
@@ -3,7 +3,7 @@ return {
     "isakbm/gitgraph.nvim",
     keys = {
       {
-        "<leader>gg",
+        "<leader>gG",
         function() require("gitgraph").draw({}, { all = true, max_count = 5000 }) end,
         desc = "Git graph",
       },
