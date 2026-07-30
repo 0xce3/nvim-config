2dd81ff5a8250274331875d4a74797283038def0
Daniel Carius <daniel.carius@codewrights.de>
2026-07-30 09:48:57 +0200

    fix: close diffview tab with enter

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -63,5 +63,21 @@ return {
         win_config = { position = "left", width = 34 },
       },
     },
+    config = function(_, opts)
+      require("diffview").setup(opts)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "DiffviewViewOpened",
+        callback = function()
+          for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            local bufnr = vim.api.nvim_win_get_buf(win)
+            vim.keymap.set("n", "<CR>", "<cmd>tabclose<cr>", {
+              buffer = bufnr,
+              silent = true,
+              desc = "Close Diffview tab",
+            })
+          end
+        end,
+      })
+    end,
   },
 }
