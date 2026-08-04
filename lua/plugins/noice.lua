c559ea17ec9b746c50d4a52d4b8f35ebbd19f372
Daniel Carius <daniel.carius@codewrights.de>
2026-08-04 14:59:19 +0200

    feat: add noice command line UI

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+      },
+      popupmenu = {
+        enabled = true,
+      },
+    },
+  },
+}
