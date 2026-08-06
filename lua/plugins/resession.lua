9140b04a9708f009eb0a6ec079c4be4a77c7deb6
Daniel Carius <daniel.carius@codewrights.de>
2026-08-06 16:37:09 +0200

    fix: preserve remote workspace after session load

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -1,6 +1,18 @@
 return {
   {
     "stevearc/resession.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "ResessionLoadPost",
+        callback = function()
+          local workspace = vim.env.NVIM_DEV_WORKSPACE
+          if vim.env.NVIM_DEV_REMOTE == "1" and workspace and vim.fn.isdirectory(workspace) == 1 then
+            vim.cmd.cd(workspace)
+          end
+        end,
+        desc = "Keep restored remote sessions in their devcontainer workspace",
+      })
+    end,
     opts = function(_, opts)
       opts.extensions = opts.extensions or {}
       opts.extensions.task_terminal = { enable_in_tab = true }
